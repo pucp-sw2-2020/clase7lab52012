06311dc485bda4387a080e579f96de42c794e2a5
lewis-coder <57689615+lewis-coder@users.noreply.github.com>
2020-05-18 12:37:15 -0500

    entidad distribuidoras

---
+++ src/main/java/pe/pucp/sw2/clase7lab52012/entity/Distribuidoras.java
@@ -0,0 +1,33 @@
+package pe.pucp.sw2.clase7lab52012.entity;
+@Entity
+@Table(name="distribuidoras")
+public class Distribuidoras {
+    @Id
+    private int iddistribuidora;
+    private String nombre;
+    private String descripcion;
+
+    public int getIddistribuidora() {
+        return iddistribuidora;
+    }
+
+    public void setIddistribuidora(int iddistribuidora) {
+        this.iddistribuidora = iddistribuidora;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
