a9c895658ec1db8a6468afeaa8dea17eff663165
Josue Romero <jcromero@pucp.edu.pe>
2020-05-18 12:40:10 -0500

    plafatormas

---
+++ src/main/java/pe/pucp/sw2/clase7lab52012/entity/Plataformas.java
@@ -0,0 +1,33 @@
+package pe.pucp.sw2.clase7lab52012.entity;
+
+import com.sun.javafx.beans.IDProperty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Plataformas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String nombre;
+    private String descripcion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
